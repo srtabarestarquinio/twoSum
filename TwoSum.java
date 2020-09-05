903b581ee9645665d4366dd6b684fa8919fa3a5f
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-09-04 20:48:20 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,64 +1,107 @@
 import java.util.*;
 
-public class TwoSum{
+public class Main{
 	//Helper functions:
-    public int findSmallest (int [] arr, int start){
+    public int findSmallest (int [] sortedArr, int start){
         int smallest = start; //assume first item is smallest
-        for (int i = start+1; i<arr.length; i++){
-            if (arr[i] < arr[smallest]){//change smallest, as necessary
+        for (int i = start+1; i<sortedArr.length; i++){
+            if (sortedArr[i] < sortedArr[smallest]){//change smallest, as necessary
                 smallest = i;
             } 
         }
         return smallest;
     }
     //helper swap funtion
-    public void swap (int [] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    public void swap (int [] sortedArr, int i, int j){
+        int temp = sortedArr[i];
+        sortedArr[i] = sortedArr[j];
+        sortedArr[j] = temp;
     }  	
-	public void sort(int []arr){
+	public int[] sort(int []sortedArr){
 		//sort unsorted array
-		int [] sortedArr
-        for (int i=0;i<arr.length-1; i++) {
-            swap(arr, i, findSmallest(arr, i));
+        //int [] sortedArr=arr;
+        for (int i=0;i<sortedArr.length-1; i++) {
+            swap(sortedArr, i, findSmallest(sortedArr, i));
         }
-		return 
+        return sortedArr;
 	}
-	public int twoSum(int[] num, int target){
-		int low=0, high=num.length()-1;
+	public int [] findTwoSum(int[] arr, int target){
+		System.out.println(Arrays.toString(arr));
+		int [] sortedArr=arr;
+		sortedArr=sort(sortedArr);
 
+		int low=0, high=sortedArr.length-1;
+
+		System.out.println(Arrays.toString(arr));
 		while(low<high){
-			int sum=num[low]+num[high];
+			int sum=sortedArr[low]+sortedArr[high];
 			if(sum==target){
-				return new int[]={low+1, high+1};
+				int arrlow=0, arrhigh=0;
+				for(int i=0; i<arr.length;i++){
+					if (arr[i]==sortedArr[low]){
+						System.out.println(arr[i]+"index: "+i);
+						arrlow=i;
+					}
+					if(arr[i]==sortedArr[high]){
+						System.out.println(arr[i]+"index:"+i);
+						arrhigh=i;
+					}
+				}
+				return new int[] {arrlow, arrhigh};
 			}
-			else if(sum>target){
-				--high;
+			else if(sum<target){
+				low++;
 			}
 			else{
-				++low;
+				high--;
 			}
 		}
+		return new int[] {};
 	}
 
 	//Main function: Welcome user, take user input
 	public static void main(String[] args){
-		TwoSum twoSum = new twoSum();
-		//initialize array of given nums
-		int [] arr={}
+		Main twoSum = new Main();
+
 		Scanner input=new Scanner(System.in);
-		System.out.print("Given nums = ");
-		System.out.println(" target = ");
+		
+		//Create array of user input size
+		System.out.println("Input size of desired array:");
+		int n=input.nextInt();
+		int [] arr=new int[n];
 
-		//replace user input array by the sorted version
-		twoSum.sort(arr);
+		//add user input values into array
+		System.out.println("Input array values:");
+		for(int i=0; i<n; i++){
+			arr[i]=input.nextInt();
+		}
+
+		//create target variable and store user input
+		System.out.println("Target value:");
+		int target = input.nextInt();
+
+		input.close();
+
+		//Print given array and target value
+		System.out.println("Given nums = "+Arrays.toString(arr)+", target= "+target);
+		
+		//Sort array
+		
+
+		int [] indices = twoSum.findTwoSum(arr, target);
+    	
+    	System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(indices));
+		//int [] indicesUnsorted = {Arrays.asList(arr).indexOf(indicesSorted[0]), Arrays.asList(arr).indexOf(indicesSorted[1])};
 
 
+		if(indices.length==2){
+			System.out.println("The output should be "+Arrays.toString(indices));
+			int idx1=indices[0], idx2=indices[1];
+					System.out.println("Because arr["+idx1+"] + arr["+idx2+"] = "+arr[idx1]+" + "+arr[idx2]+" ="+(arr[idx1]+arr[idx2]));
 
-
-		System.out.println("The output should be ");
-		System.out.println("Because nums[] + nums[] = ");
+		}
+		
 	}
 }
 
